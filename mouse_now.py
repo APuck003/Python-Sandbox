351760ba60c2082b77cd9abf84d8b9c432269b12
Austin Puckett <puckett_austin@yahoo.com>
2019-06-14 23:16:54 -0500

    Update mouse_now.py

--- mouse_now.py
+++ mouse_now.py
@@ -11,3 +11,7 @@ try:
     positionStr = 'X: ' + str(x).rjust(4) + 'Y: ' + str(y).rjust(4)
     print(positionStr, end='')
     print('\b' * len(positionStr), end='', flush=True)
+
+except KeyboardInterrupt:
+  print('\nDone.')
+    
